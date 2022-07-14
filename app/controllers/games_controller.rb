8bc93397d6a4cf96afa0885d68d3a91e8d42aecb
Aaron Lambley <aaron.lambley@gmail.com>
2022-07-14 19:58:45 +0100

    updated new method in games constructor, to include high_score variable. updated score and new html pages to pass high_score between pages as params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,18 +6,27 @@ WAGON_URL = 'https://wagon-dictionary.herokuapp.com/'.freeze
 # Game logic for /new and /score pages
 class GamesController < ApplicationController
   def new
-    # display new random grid and a form
+    # display new random grid and a form => array
     @letters = (1..10).map { ('a'..'z').to_a[rand(26)] }
-    # form will be submitted with POST to the score action
+    # start time to pass to score method => float
+    @start_time = Time.now.to_f
+    # form will be submitted with POST to the score action => integer
+    @high_score = params[:score].to_i > params[:high_score].to_i ? params[:score].to_i : params[:high_score]
   end
 
   def score
-    # calculate score
-    @word = params[:word]
+    # submit time, rounded to 2dp => float
+    @time_taken = (Time.now.to_f - params[:start_time].to_f).round(2)
+    # player's word attempt, downcased => string
+    @word = params[:word].downcase
+    # that round's letters => array
     @grid = params[:grid].split(' ')
-    # return bool: does word exist or not
-    @result = dictionary_lookup(@word)
+    # check if word exist in dictionary and grid => bool
+    @result = exists_in_dictionary?(@word) && word_in_grid?(@word.chars, @grid)
+    # message to player based on if the word exists in dictionary and in grid => string
     @result_message = result_message(@result, @word)
+    # score value based on time and length => integer
+    @score = @result ? calculate_score(@word, @time_taken) : 0
   end
 
   private
@@ -28,11 +37,16 @@ class GamesController < ApplicationController
   end
 
   def result_message(result, word)
-    result ? "#{word} is valid" : "sorry, #{word} is not valid"
+    result ? "Congrats! #{word.capitalize} is a valid word" : "Sorry, #{word} is not valid. Please try again"
   end
 
-  def dictionary_lookup(word)
+  def exists_in_dictionary?(word)
     res = JSON.parse(URI.open("#{WAGON_URL}#{word}").read)
     res['found']
+  end
+
+  def calculate_score(word, time)
+    base_score = (word.length / time) * 1000
+    base_score.round
   end
 end
